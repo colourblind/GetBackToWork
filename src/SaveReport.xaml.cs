14282cc6a24d4b1bd53c8e401da5525fc8ae1f6e
colourblind <tom.milsom@gmail.com>
2010-10-12 15:08:16 +0100

    More reporting WIP

--- src/SaveReport.xaml.cs
+++ src/SaveReport.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace GetBackToWork
 {
@@ -8,19 +9,29 @@ namespace GetBackToWork
         public SaveReport()
         {
             InitializeComponent();
+
+            DateTime now = DateTime.Now;
+            StartDatePicker.SelectedDate = new DateTime(now.Year, now.Month, 1);
+            EndDatePicker.SelectedDate = new DateTime(now.Year, now.Month, DateTime.DaysInMonth(now.Year, now.Month));
         }
 
         private void SaveButton_Click(object sender, RoutedEventArgs e)
         {
-            int year = DateTime.Now.Year;
-            int month = DateTime.Now.Month;
-
-            DateTime start = new DateTime(year, month, 1);
-            DateTime end = new DateTime(year, month, DateTime.DaysInMonth(year, month));
-
-            Report report = new Report(DateTime.Now.ToString("yyyy-MM") + ".csv", start, end);
+            Report report = new Report(DateTime.Now.ToString("yyyy-MM") + ".csv", (DateTime)StartDatePicker.SelectedDate, (DateTime)EndDatePicker.SelectedDate);
 
             report.Create();
+        }
+
+        private void StartDatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (StartDatePicker.SelectedDate > EndDatePicker.SelectedDate)
+                EndDatePicker.SelectedDate = StartDatePicker.SelectedDate;
+        }
+
+        private void EndDatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (EndDatePicker.SelectedDate < StartDatePicker.SelectedDate)
+                StartDatePicker.SelectedDate = EndDatePicker.SelectedDate;
         }
     }
 }
